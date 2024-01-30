b32e113f42f34c7ee26420d7feba89935e4b2c4e
Bepractical1 <kartivesingh@gmail.com>
2024-01-30 14:38:53 +0530

    PRASHANT

---
+++ form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form ='info@oyurwebsite.com';
+
+$email_subject ='New Form Submission';
+
+
+$email_body ="User Name: $name.\n".
+             "User Email: $visitor_email.\n".
+             "Subject: $subject.\n".
+             "User Message: $message.\n".
+ $to ='dadimasram@gmail.com';
+ 
+ $headers ="From: $emaili_from \r\n";
+
+ $headers .="Reply0-To: $visitor_email \r\n";
+
+
+ mail($to,$email_subject,$email_body,$header);
+
+header("location: contact.html");
+
+
+
+?>
